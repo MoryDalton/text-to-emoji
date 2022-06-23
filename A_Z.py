ba715e1ceb76de72f79bb49eb9e0ccca3c596a44
mory <mory>
2022-06-23 21:29:09 +0430

    V1

---
+++ A_Z.py
@@ -0,0 +1,54 @@
+def text_to_emoji(text, elm):
+    all_words = {
+        'A': [f'      {elm}      ', f'     {elm} {elm}     ', f'    {elm}   {elm}    ', f'   {elm} {elm} {elm} {elm}   ', f'  {elm}       {elm}  ', f' {elm}         {elm} ', f'{elm}           {elm}'],
+        'B': [f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}   '],
+        'C': [f'   {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'{elm}            ', f'{elm}            ', f'{elm}            ', f'   {elm}  {elm}  {elm}  {elm}'],
+        'D': [f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}   '],
+        'E': [f'{elm}  {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'{elm}  {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'{elm}  {elm}  {elm}  {elm}  {elm}'],
+        'F': [f'{elm}  {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'{elm}  {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'{elm}            '],
+        'G': [f'{elm}  {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'{elm}     {elm}  {elm}  {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}  {elm}'],
+        'H': [f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}  {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}'],
+        'I': [f'   {elm}  {elm}  {elm}   ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'   {elm}  {elm}  {elm}   '],
+        'J': [f'   {elm}  {elm}  {elm}   ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'{elm}     {elm}      ', f'{elm}  {elm}  {elm}      '],
+        'K': [f'{elm}           {elm}', f'{elm}        {elm}   ', f'{elm}     {elm}      ', f'{elm}  {elm}         ', f'{elm}     {elm}      ', f'{elm}        {elm}   ', f'{elm}           {elm}'],
+        'L': [f'{elm}            ', f'{elm}            ', f'{elm}            ', f'{elm}            ', f'{elm}            ', f'{elm}            ', f'{elm}  {elm}  {elm}  {elm}  {elm}'],
+        'M': [f'{elm}           {elm}', f'{elm} {elm}       {elm} {elm}', f'{elm}   {elm}   {elm}   {elm}', f'{elm}     {elm}     {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}'],
+        'N': [f'{elm}           {elm}', f'{elm} {elm}         {elm}', f'{elm}   {elm}       {elm}', f'{elm}     {elm}     {elm}', f'{elm}       {elm}   {elm}', f'{elm}         {elm} {elm}', f'{elm}           {elm}'],
+        'O': [f'   {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'   {elm}  {elm}  {elm}   '],
+        'P': [f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}            ', f'{elm}            ', f'{elm}            '],
+        'Q': [f'   {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}      {elm}    {elm}', f'{elm}        {elm}   ', f'   {elm}  {elm}     {elm}'],
+        'R': [f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}  {elm}  {elm}  {elm}   ', f'{elm}  {elm}         ', f'{elm}      {elm}     ', f'{elm}          {elm} '],
+        'S': [f'   {elm}  {elm}  {elm}  {elm}', f'{elm}            ', f'{elm}            ', f'   {elm}  {elm}  {elm}   ', f'            {elm}', f'            {elm}', f'{elm}  {elm}  {elm}  {elm}   '],
+        'T': [f'{elm}  {elm}  {elm}  {elm}  {elm}', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      '],
+        'U': [f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'   {elm}  {elm}  {elm}   '],
+        'V': [f'{elm}           {elm}', f' {elm}         {elm} ', f'  {elm}       {elm}  ', f'   {elm}     {elm}   ', f'    {elm}   {elm}    ', f'     {elm} {elm}     ', f'      {elm}      '],
+        'W': [f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}           {elm}', f'{elm}     {elm}     {elm}', f'{elm}   {elm}   {elm}   {elm}', f'{elm} {elm}       {elm} {elm}', f'{elm}           {elm}'],
+        'X': [f'{elm}           {elm}', f'  {elm}       {elm}  ', f'    {elm}   {elm}    ', f'      {elm}      ', f'    {elm}   {elm}    ', f'  {elm}       {elm}  ', f'{elm}           {elm}'],
+        'Y': [f'{elm}           {elm}', f'  {elm}       {elm}  ', f'    {elm}   {elm}    ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      ', f'      {elm}      '],
+        'Z': [f'{elm}  {elm}  {elm}  {elm}  {elm}', f'          {elm}  ', f'        {elm}    ', f'      {elm}      ', f'    {elm}        ', f'  {elm}          ', f'{elm}  {elm}  {elm}  {elm}  {elm}']
+    }
+
+    t = list(text)
+    l = []
+
+    for i in t:
+
+        l.append(all_words.get(i, i))
+
+    return show(l)
+
+
+def show(l):
+
+    result = '\n'
+    for i in range(7):
+
+        s = ''
+
+        for j in range(len(l)):
+
+            s += l[j][i]+(5*' ')
+
+        result += s+'\n'
+
+    return result
---
+++ start.py
@@ -0,0 +1,26 @@
+from A_Z import text_to_emoji
+from string import ascii_uppercase
+
+
+def check_text():
+
+    text = input('text:').upper()
+
+    for item in text:
+        if item not in ascii_uppercase:
+
+            print('invalid text')
+            return check_text()
+
+    elm = input('element:')
+
+    if len(elm) > 1:
+
+        print('invalid element')
+        return check_text()
+
+    return text_to_emoji(text, elm)
+
+
+if __name__ == ('__main__'):
+    print(check_text())
